855ffeded8c9dc1297d0383bc63d604c9f7370da
秋雨落 <i@rain.cx>
2023-03-08 09:20:26 +0800

    chore: Init SCA. why we should launch game from the root project?
    Signed-off-by: 秋雨落 <i@rain.cx>

---
+++ SinoCalligraphy/src/main/java/games/moegirl/sinocraft/sinocalligraphy/SinoCalligraphy.java
@@ -0,0 +1,15 @@
+package games.moegirl.sinocraft.sinocalligraphy;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(SinoCalligraphy.MODID)
+public class SinoCalligraphy {
+    public static final String MODID = "sinocalligraphy";
+
+    public SinoCalligraphy() {
+        var bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        System.out.println("Hello sca!");
+    }
+}
