8bd3984df365d2696c7f896a40538aad088cdb91
luqin2007 <lq2007lq@hotmail.com>
2023-03-14 01:05:04 +0800

    fix: error

--- SinoCore/src/main/java/games/moegirl/sinocraft/sinocore/old/network/SCNetworks.java
+++ SinoCore/src/main/java/games/moegirl/sinocraft/sinocore/old/network/SCNetworks.java
@@ -5,9 +5,11 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class SCNetworks {
 
-    public static final Id ID = new Id();
+    public static final AtomicInteger ID = new AtomicInteger();
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(new ResourceLocation(SinoCore.MODID, "common"), () -> "1", __ -> true, __ -> true);
 
     public static void register() {}
