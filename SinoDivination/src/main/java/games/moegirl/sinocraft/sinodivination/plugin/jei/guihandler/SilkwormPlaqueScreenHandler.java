4a7135cf3c66292ef2490014690463551cc7c87e
秋雨落 <i@rain.cx>
2023-07-01 14:22:33 +0800

    fix: sub projects build.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- SinoDivination/src/main/java/games/moegirl/sinocraft/sinodivination/plugin/jei/guihandler/SilkwormPlaqueScreenHandler.java
+++ SinoDivination/src/main/java/games/moegirl/sinocraft/sinodivination/plugin/jei/guihandler/SilkwormPlaqueScreenHandler.java
@@ -3,18 +3,23 @@ package games.moegirl.sinocraft.sinodivination.plugin.jei.guihandler;
 import games.moegirl.sinocraft.sinodivination.client.screen.SilkwormPlaqueScreen;
 import mezz.jei.api.gui.handlers.IGuiProperties;
 import mezz.jei.api.gui.handlers.IScreenHandler;
-import mezz.jei.gui.GuiProperties;
 import net.minecraft.client.Minecraft;
+import org.jetbrains.annotations.Nullable;
 
 public enum SilkwormPlaqueScreenHandler implements IScreenHandler<SilkwormPlaqueScreen> {
 
     INSTANCE;
 
+//    @Override
+//    public IGuiProperties apply(SilkwormPlaqueScreen guiScreen) {
+//        Minecraft mc = Minecraft.getInstance();
+//        return new GuiProperties(SilkwormPlaqueScreen.class,
+//                guiScreen.getGuiLeft(), guiScreen.getGuiTop(), guiScreen.width, guiScreen.height,
+//                mc.getWindow().getScreenWidth(), mc.getWindow().getScreenHeight());
+//    }
+
     @Override
-    public IGuiProperties apply(SilkwormPlaqueScreen guiScreen) {
-        Minecraft mc = Minecraft.getInstance();
-        return new GuiProperties(SilkwormPlaqueScreen.class,
-                guiScreen.getGuiLeft(), guiScreen.getGuiTop(), guiScreen.width, guiScreen.height,
-                mc.getWindow().getScreenWidth(), mc.getWindow().getScreenHeight());
+    public @Nullable IGuiProperties apply(SilkwormPlaqueScreen guiScreen) {
+        return null;
     }
 }
