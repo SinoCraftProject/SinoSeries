5a7e4361964b11b3df2eaff56439fd228a4c3cd3
luqin2007 <lq2007lq@hotmail.com>
2024-02-03 14:19:52 +0800

    fix: sb tab 初始化问题

--- sinobrush/src/main/java/games/moegirl/sinocraft/sinobrush/item/SBRItems.java
+++ sinobrush/src/main/java/games/moegirl/sinocraft/sinobrush/item/SBRItems.java
@@ -15,14 +15,14 @@ public class SBRItems {
     public static IRegistry<Item> ITEMS = RegistryManager.obtain(SinoBrush.MODID, Registries.ITEM);
     public static ITabRegistry TABS = RegistryManager.obtainTab(SinoBrush.MODID);
 
+    public static ResourceKey<CreativeModeTab> SINO_BRUSH_TAB = TABS.register("sinobrush");
+
     public static Supplier<Item> FAN = ITEMS.register("fan", FanItem::new);
     public static Supplier<Item> FOLDED_FAN = ITEMS.register("folded_fan", FoldedFanItem::new);
     public static Supplier<Item> XUAN_PAPER = ITEMS.register("xuan_paper", () -> new XuanPaperItem(new Item.Properties().sino$tab(SBRItems.SINO_BRUSH_TAB)));
     public static Supplier<Item> FILLED_XUAN_PAPER = ITEMS.register("filled_xuan_paper", FilledXuanPaperItem::new);
     public static Supplier<Item> INK = ITEMS.register("ink", InkItem::new);
     public static Supplier<Item> BRUSH = ITEMS.register("brush", InkItem::new);
-
-    public static ResourceKey<CreativeModeTab> SINO_BRUSH_TAB = TABS.register("sinobrush");
 
     public static void register() {
         ITEMS.register();
