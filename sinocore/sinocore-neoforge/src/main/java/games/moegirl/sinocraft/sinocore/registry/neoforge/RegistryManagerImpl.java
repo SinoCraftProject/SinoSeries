4848ca5c7c6e09710f41a883bcbb7bfc5316ced6
luqin2007 <lq2007lq@hotmail.com>
2024-02-03 14:12:33 +0800

    fix: dg 注册时非 Forge 端崩溃

--- sinocore/sinocore-neoforge/src/main/java/games/moegirl/sinocraft/sinocore/registry/neoforge/RegistryManagerImpl.java
+++ sinocore/sinocore-neoforge/src/main/java/games/moegirl/sinocraft/sinocore/registry/neoforge/RegistryManagerImpl.java
@@ -1,10 +1,15 @@
 package games.moegirl.sinocraft.sinocore.registry.neoforge;
 
+import games.moegirl.sinocraft.sinocore.datagen.IDataGenContext;
 import games.moegirl.sinocraft.sinocore.registry.IDataProviderRegister;
 import games.moegirl.sinocraft.sinocore.registry.IRegistry;
 import games.moegirl.sinocraft.sinocore.registry.ITabRegistry;
 import net.minecraft.core.Registry;
+import net.minecraft.data.DataProvider;
 import net.minecraft.resources.ResourceKey;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class RegistryManagerImpl {
 
@@ -17,6 +22,12 @@ public class RegistryManagerImpl {
     }
 
     public static IDataProviderRegister _createDataProvider(String modId) {
-        throw new IllegalStateException("Data Provider only for forge platform.");
+        return new IDataProviderRegister() {
+
+            @Override
+            public <T extends DataProvider> Supplier<T> put(Function<IDataGenContext, ? extends T> builder, boolean run) {
+                return () -> null;
+            }
+        };
     }
 }
