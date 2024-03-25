c861638277c4d38360fb4fc2b86391f4ff491719
luqin2007 <lq2007lq@hotmail.com>
2024-03-25 22:24:07 +0800

    feat: 移除部分没用的方法

--- sinocore/src/main/java/games/moegirl/sinocraft/sinocore/utility/EasyCast.java
+++ sinocore/src/main/java/games/moegirl/sinocraft/sinocore/utility/EasyCast.java
@@ -16,28 +16,4 @@ public interface EasyCast {
     static <T> Optional<T> safeCast(Object o, Class<? extends T> aClass) {
         return aClass.isInstance(o) ? Optional.of((T) o) : Optional.empty();
     }
-
-    default <T> T cast() {
-        return forceCast(this);
-    }
-
-    interface Self<T> extends EasyCast {
-
-        default T self() {
-            return safeCast(this, selfType()).orElseThrow();
-        }
-
-        default Class<T> selfType() {
-            for (Type genericInterface : getClass().getGenericInterfaces()) {
-                if (genericInterface instanceof ParameterizedType pType && pType.getRawType() == Self.class) {
-                    Type argumentType = pType.getActualTypeArguments()[0];
-                    if (argumentType instanceof Class<?> aClass) {
-                        return forceCast(aClass);
-                    }
-                }
-                throw new IllegalArgumentException("Type of T in Self<T> is not a Class.");
-            }
-            throw new RuntimeException("Object must implement Self interface (but why not?)");
-        }
-    }
 }
