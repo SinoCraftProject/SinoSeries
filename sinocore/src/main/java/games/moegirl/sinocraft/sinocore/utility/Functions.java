4ffd83116e61fa14e801c293f0a5c4e52183e825
luqin2007 <lq2007lq@hotmail.com>
2024-03-25 22:24:07 +0800

    feat: 移除部分没用的方法

--- sinocore/src/main/java/games/moegirl/sinocraft/sinocore/utility/Functions.java
+++ sinocore/src/main/java/games/moegirl/sinocraft/sinocore/utility/Functions.java
@@ -4,9 +4,8 @@ import org.apache.logging.log4j.Logger;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.Callable;
-import java.util.function.BiFunction;
 import java.util.Optional;
+import java.util.concurrent.Callable;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -16,13 +15,6 @@ import java.util.stream.Stream;
  * 提供一些函数功能
  */
 public class Functions {
-
-    /**
-     * 生成一个 Supplier，可以根据给定构造函数构造出无参构造，并通过 Function 对其修饰
-     */
-    public static <T, R> Supplier<R> constructor(Supplier<T> constructor, Function<T, R> decorator) {
-        return () -> decorator.apply(constructor.get());
-    }
 
     /**
      * 将一个无参构造转换成 Supplier
@@ -36,14 +28,6 @@ public class Functions {
             } catch (Exception e) {
                 throw new IllegalArgumentException("Can't create " + aClass.getCanonicalName() + " by no-parameter public constructor.", e);
             }
-        };
-    }
-
-    public static <T, R> Function<T, R> compose(Function<T, R> first, Consumer<R> second) {
-        return t -> {
-            R tt = first.apply(t);
-            second.accept(tt);
-            return tt;
         };
     }
 
@@ -74,20 +58,6 @@ public class Functions {
         } catch (NoSuchMethodException ex) {
             throw new IllegalArgumentException("Can't find " + aClass.getCanonicalName() + "(" + p1.getName() + ")", ex);
         }
-    }
-
-    public static <T, R> Function<T, R> toFunction(FunctionCallable<T, R> callable) {
-        return t -> {
-            try {
-                return callable.call(t);
-            } catch (Exception e) {
-                throw new IllegalArgumentException("Failed to call function with " + t, e);
-            }
-        };
-    }
-
-    public interface FunctionCallable<T, R> {
-        R call(T t) throws Exception;
     }
 
     public static <P> Optional<P> getOrEmpty(Callable<P> sup, Logger logger) {
