c1b789d45b24aff9f2bc7bbb45f1906aa565959e
秋雨落 <i@rain.cx>
2024-05-26 19:05:51 +0800

    feat: dyeable paper and ink bottle.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- sinobrush/sinobrush-fabric/src/main/java/games/moegirl/sinocraft/sinobrush/fabric/SinoBrushFabric.java
+++ sinobrush/sinobrush-fabric/src/main/java/games/moegirl/sinocraft/sinobrush/fabric/SinoBrushFabric.java
@@ -1,8 +1,11 @@
 package games.moegirl.sinocraft.sinobrush.fabric;
 
 import games.moegirl.sinocraft.sinobrush.SinoBrush;
+import games.moegirl.sinocraft.sinobrush.item.SBRItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.minecraft.world.item.DyeableLeatherItem;
 
 public class SinoBrushFabric implements ModInitializer, ClientModInitializer {
 
@@ -16,5 +19,12 @@ public class SinoBrushFabric implements ModInitializer, ClientModInitializer {
     @Override
     public void onInitializeClient() {
         mod.initClient();
+
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
+            if (tintIndex == 0 && stack.getItem() instanceof DyeableLeatherItem dyeable) {
+                return dyeable.getColor(stack);
+            }
+            return -1;
+        }, SBRItems.XUAN_PAPER.get(), SBRItems.FILLED_XUAN_PAPER.get(), SBRItems.INK_BOTTLE.get());
     }
 }
---
+++ sinobrush/sinobrush-forge/src/main/java/games/moegirl/sinocraft/sinobrush/forge/listener/RegisterColorHandlersListener.java
@@ -0,0 +1,22 @@
+package games.moegirl.sinocraft.sinobrush.forge.listener;
+
+import games.moegirl.sinocraft.sinobrush.SinoBrush;
+import games.moegirl.sinocraft.sinobrush.item.SBRItems;
+import net.minecraft.world.item.DyeableLeatherItem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SinoBrush.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class RegisterColorHandlersListener {
+    @SubscribeEvent
+    public static void onRegisterColorHandlersEvent(RegisterColorHandlersEvent.Item event) {
+        event.register((stack, tintIndex) -> {
+            if (tintIndex == 0 && stack.getItem() instanceof DyeableLeatherItem dyeable) {
+                return dyeable.getColor(stack);
+            }
+            return -1;
+        }, SBRItems.XUAN_PAPER.get(), SBRItems.FILLED_XUAN_PAPER.get(), SBRItems.INK_BOTTLE.get());
+    }
+}
