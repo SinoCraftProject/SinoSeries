317a8deb1766ffd5b1dfff0992a8c4878593d388
秋雨落 <i@rain.cx>
2024-05-26 18:11:33 +0800

    :sparkles: Update model.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- sinodeco/src/main/java/games/moegirl/sinocraft/sinodeco/data/gen/SDData.java
+++ sinodeco/src/main/java/games/moegirl/sinocraft/sinodeco/data/gen/SDData.java
@@ -6,6 +6,10 @@ import games.moegirl.sinocraft.sinodeco.SinoDeco;
 public class SDData {
     public static void register() {
         var register = RegistryManager.obtainDataProvider(SinoDeco.MODID);
-        // Todo: qyl27: block state provider without forge.
+
+//        var blockTagProvider = register.put(context -> new BlockTagProvider(context.getOutput(), context.registriesFuture()));
+//        register.put(context -> new ItemTagProvider(context, blockTagProvider.get()));
+//
+//        register.put(context -> new ItemModelProvider(context, SBRItems.ITEMS));
     }
 }
