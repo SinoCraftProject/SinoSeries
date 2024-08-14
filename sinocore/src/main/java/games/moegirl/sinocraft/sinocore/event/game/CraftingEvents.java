82cb5217b786209b3a9a5153e2c7ba23d6905818
秋雨落 <i@rain.cx>
2024-08-14 18:15:39 +0800

    feat: CraftingEvents.CARTOGRAPHY_CRAFT .
    Signed-off-by: 秋雨落 <i@rain.cx>

--- sinocore/src/main/java/games/moegirl/sinocraft/sinocore/event/game/CraftingEvents.java
+++ sinocore/src/main/java/games/moegirl/sinocraft/sinocore/event/game/CraftingEvents.java
@@ -3,9 +3,9 @@ package games.moegirl.sinocraft.sinocore.event.game;
 import games.moegirl.sinocraft.sinocore.event.EventFactory;
 import games.moegirl.sinocraft.sinocore.event.ICancellableEventHandler;
 import games.moegirl.sinocraft.sinocore.event.IEvent;
-import games.moegirl.sinocraft.sinocore.event.game.args.CartographyCraftEventArgs;
+import games.moegirl.sinocraft.sinocore.event.game.args.crafting.CartographyTableCraftEventArgs;
 
 public class CraftingEvents {
-    public static final IEvent<CartographyCraftEventArgs, ICancellableEventHandler<CartographyCraftEventArgs>> CARTOGRAPHY_CRAFT = EventFactory.createCancellableEvent(CartographyCraftEventArgs.class);
+    public static final IEvent<CartographyTableCraftEventArgs, ICancellableEventHandler<CartographyTableCraftEventArgs>> CARTOGRAPHY_CRAFT = EventFactory.createCancellableEvent(CartographyTableCraftEventArgs.class);
 
 }
