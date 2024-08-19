38febd6072725b9f4c50686e97ed5e144fa790ec
秋雨落 <i@rain.cx>
2024-08-19 19:38:49 +0800

    fix: a shape mismatch and rename.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- sinodeco/sinodeco-forge/src/main/java/games/moegirl/sinocraft/sinodeco/data/gen/forge/ModBlockStateProvider.java
+++ sinodeco/sinodeco-forge/src/main/java/games/moegirl/sinocraft/sinodeco/data/gen/forge/ModBlockStateProvider.java
@@ -1,7 +1,7 @@
 package games.moegirl.sinocraft.sinodeco.data.gen.forge;
 
 import games.moegirl.sinocraft.sinodeco.block.SDBlocks;
-import games.moegirl.sinocraft.sinodeco.block.WoodDeskBlock;
+import games.moegirl.sinocraft.sinodeco.block.WoodenTableBlock;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -16,13 +16,13 @@ public class ModBlockStateProvider extends BlockStateProvider {
         {
             var builder = getMultipartBuilder(SDBlocks.WOODEN_TABLE.get());
 
-            for (var i = 0; i < WoodDeskBlock.PARTS.length; i++) {
+            for (var i = 0; i < WoodenTableBlock.PARTS.length; i++) {
                 var north = (i & 0b1000) != 0;
                 var east = (i & 0b0100) != 0;
                 var south = (i & 0b0010) != 0;
                 var west = (i & 0b0001) != 0;
 
-                var extras = WoodDeskBlock.PARTS[i];
+                var extras = WoodenTableBlock.PARTS[i];
                 for (var parts : extras) {
                     if (parts == null) {
                         continue;
@@ -34,13 +34,13 @@ public class ModBlockStateProvider extends BlockStateProvider {
                         }
 
                         var part = builder.part()
-                                .modelFile(models().getExistingFile(modLoc("block/wood_desk_" + p.name())))
+                                .modelFile(models().getExistingFile(modLoc("block/wooden_table_" + p.name())))
                                 .rotationY(p.rotate())
                                 .addModel()
-                                .condition(WoodDeskBlock.NORTH, north)
-                                .condition(WoodDeskBlock.EAST, east)
-                                .condition(WoodDeskBlock.SOUTH, south)
-                                .condition(WoodDeskBlock.WEST, west);
+                                .condition(WoodenTableBlock.NORTH, north)
+                                .condition(WoodenTableBlock.EAST, east)
+                                .condition(WoodenTableBlock.SOUTH, south)
+                                .condition(WoodenTableBlock.WEST, west);
 
                         if (p.condition() != null) {
                             part.condition(p.condition(), p.conditionValue());
