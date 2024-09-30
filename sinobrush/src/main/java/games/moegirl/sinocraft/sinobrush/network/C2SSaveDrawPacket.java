93534e58ae680c3ad1c1d76218a55e5285630929
秋雨落 <i@rain.cx>
2024-09-30 14:47:08 +0800

    fix: compile mistake.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- sinobrush/src/main/java/games/moegirl/sinocraft/sinobrush/network/C2SSaveDrawPacket.java
+++ sinobrush/src/main/java/games/moegirl/sinocraft/sinobrush/network/C2SSaveDrawPacket.java
@@ -19,12 +19,13 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.ZonedDateTime;
 
-public record C2SSaveDrawPacket(MutableDrawing drawing, EquipmentSlot brushSlot) implements CustomPacketPayload {
+public record C2SSaveDrawPacket(MutableDrawing drawing, int brushSlot) implements CustomPacketPayload {
 
     public static final Type<C2SSaveDrawPacket> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(SinoBrush.MODID, "save_draw"));
 
@@ -32,13 +33,13 @@ public record C2SSaveDrawPacket(MutableDrawing drawing, EquipmentSlot brushSlot)
         @Override
         public void encode(RegistryFriendlyByteBuf buf, C2SSaveDrawPacket packet) {
             MutableDrawing.STREAM_CODEC.encode(buf, packet.drawing());
-            buf.writeEnum(packet.brushSlot());
+            buf.writeVarInt(packet.brushSlot());
         }
 
         @Override
         public @NotNull C2SSaveDrawPacket decode(RegistryFriendlyByteBuf buf) {
             return new C2SSaveDrawPacket(MutableDrawing.STREAM_CODEC.decode(buf),
-                    buf.readEnum(EquipmentSlot.class));
+                    buf.readVarInt());
         }
     };
 
@@ -59,7 +60,7 @@ public record C2SSaveDrawPacket(MutableDrawing drawing, EquipmentSlot brushSlot)
                 NetworkManager.send(S2CDrawResultPacket.noInk(), player);
             } else if (!container.getItem(BrushMenu.DRAW_SLOT).isEmpty()) {
                 NetworkManager.send(S2CDrawResultPacket.hasDraw(), player);
-            } else if (!player.getItemBySlot(brushSlot).is(SBRItems.BRUSH.get())) {
+            } else if (!player.getInventory().getItem(brushSlot).is(SBRItems.BRUSH.get())) {
                 NetworkManager.send(S2CDrawResultPacket.noBrush(), player);
             } else {
                 drawing.setAuthor(player);
@@ -79,8 +80,8 @@ public record C2SSaveDrawPacket(MutableDrawing drawing, EquipmentSlot brushSlot)
                 container.setItem(BrushMenu.INK_SLOT, inkSlot);
 
                 if (!player.isCreative()) {
-                    var brush = player.getItemBySlot(brushSlot);
-                    brush.hurtAndBreak(1, player, brushSlot);
+                    var brush = player.getInventory().getItem(brushSlot);
+                    brush.hurtAndBreak(1, player, brushSlot == Inventory.SLOT_OFFHAND ? EquipmentSlot.OFFHAND : EquipmentSlot.MAINHAND);
                 }
 
                 ItemStack drawStack = new ItemStack(SBRItems.FILLED_XUAN_PAPER.get());
